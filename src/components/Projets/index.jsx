51340c2ca815f069b1c94e9dd2c87c8b2045e541
lucaszebre <yakirisk@gmail.com>
2023-05-11 11:05:57 +0200

    add new project and delete one

--- src/components/Projets/index.jsx
+++ src/components/Projets/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import galleria from '../../../public/assets/images/thumbnail-project-6-large.webp'
 import MyTeam from '../../../public/assets/Myteam.jpg'
 import Kanban from '../../../public/assets/Kanban.jpg'
-import Photosnap from '../../../public/assets/Photosnap.jpg'
+import Invoice from '../../../public/assets/Invoice.jpg'
 import Audiophile from '../../../public/assets/Audiophile.jpg'
 import Designo from '../../../public/assets/Designo.jpg'
 
@@ -26,18 +26,18 @@ const Projets = (props) => {
                         <div className="ProjetsTechno">NEXT.JS FIREBASE TYPESCRIPT MATERIAL UI </div>
                     </div>
                     <div className="ProjetsCard">
-                        <a href="https://lucaszebre.github.io/AudiophileEcommerce/#/" target="_blank"><img className="ImageProjets"src={Audiophile} alt="AudioPhile-website" /></a>
-                        <div className="ProjetsName">Audiophile E-Commerce Website</div>
-                        <div className="ProjetsTechno">React.js JAVASCRIPT </div>
+                        <a href="https://invoice-lovat.vercel.app/" target="_blank"><img className="ImageProjets"src={Invoice} alt="Invoice-App" /></a>
+                        <div className="ProjetsName">Invoice App </div>
+                        <div className="ProjetsTechno">NEXT.JS FIREBASE TYPESCRIPT MATERIAL UI</div>
                     </div>
                     
                     </div>
                     
                     <div className="ProjetsRow">
                     <div className="ProjetsCard">
-                        <a href="https://lucaszebre.github.io/Photosnap-multi-page-website/" target="_blank"><img className="ImageProjets"src={Photosnap} alt="PHOTOSNAP" /></a>
-                        <div className="ProjetsName">PHOTOSNAP</div>
-                        <div className="ProjetsTechno">React.JS JAVASCRIPT</div>
+                        <a href="https://lucaszebre.github.io/AudiophileEcommerce/#/" target="_blank"><img className="ImageProjets"src={Audiophile} alt="AudioPhile-website" /></a>
+                        <div className="ProjetsName">Audiophile E-Commerce Website</div>
+                        <div className="ProjetsTechno">React.js JAVASCRIPT </div>
                     </div>
                     <div className="ProjetsCard">
                         <a href="https://lucaszebre.github.io/Designo/#/" target="_blank"><img  className="ImageProjets" src={Designo} alt="EntertainnementWeb-app" /></a>
