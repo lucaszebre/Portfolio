6d5a3e805b4b0534291544815edb7d40a9279ef2
lucaszebre <yakirisk@gmail.com>
2024-04-26 13:20:51 +0200

    add resend api

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,26 @@
+import { EmailTemplate } from '@/components/EmailTemplate';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+import * as React from 'react';
+
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
+      subject: "Hello world",
+      react: EmailTemplate({ firstName: "John" }) as React.ReactElement,
+    });
+
+    if (error) {
+      return Response.json({ error });
+    }
+
+    return Response.json({ data });
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
