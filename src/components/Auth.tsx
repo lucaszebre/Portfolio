fd463e50054832d8e62c8a7240f5a00a1fa00932
lucaszebre <yakirisk@gmail.com>
2024-04-30 13:43:09 +0200

    gonna TinaCMS for the blog part

---
+++ src/components/Auth.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import React from 'react';
+import { Login } from './Login'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import dynamic from 'next/dynamic';
+import Register from './Register';
+
+function Auth() {
+
+  return (
+    
+   
+
+    <div className=" relative mt-5 mb-5 w-full h-screen flex col content-center items-center justify-center ">
+      
+       
+      <div className="relative z-20 hidden items-center text-lg font-medium md:flex">
+          
+          
+        </div>
+        
+        <Tabs defaultValue="register" className="max-w-[400px]">
+  <TabsList>
+    <TabsTrigger value="login">Login</TabsTrigger>
+    <TabsTrigger value="register">Register</TabsTrigger>
+  </TabsList>
+  <TabsContent value="login"><Login /></TabsContent>
+  <TabsContent value="register"><Register /></TabsContent>
+</Tabs>
+          
+        </div>
+
+    
+    
+  )
+}
+
+
+export default Auth
