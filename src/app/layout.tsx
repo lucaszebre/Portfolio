ce728e8edf0e9c968b4e07fb0e402aab97e43e6f
Lucas Zebre <yakirisk@gmail.com>
2024-08-13 13:45:44 +0200

    add google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { GoogleTagManager } from '@next/third-parties/google' 
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
           <QueryClientProvider client={queryClient}>
+            <GoogleTagManager gtmId="G-RL71428SMP" />
             
             <body className={inter.className}>{children}</body>
           </QueryClientProvider>
