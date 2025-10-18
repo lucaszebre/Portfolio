eebad38a5845f0a070bb85cf08f588b9d728aec9
lucaszebre <lucaszebre1@gmail.com>
2025-10-18 13:32:38 +0200

    fix: remove Google Fonts import and integrate Space Grotesk font directly in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,19 @@
 import ReactQueryProvider from "@/providers/ReactProvidersQuery";
 import { GoogleTagManager } from "@next/third-parties/google";
+import { Space_Grotesk } from "next/font/google";
+
+import { cn } from "@/lib/utils";
 import { NextIntlClientProvider } from "next-intl";
 import { getLocale, getMessages } from "next-intl/server";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space-grotesk",
+});
 
 // export const metadata: Metadata = {
 //   title: "lucaszebre",
@@ -29,7 +37,9 @@ export default async function RootLayout({
         <ReactQueryProvider>
           <GoogleTagManager gtmId="G-RL71428SMP" />
 
-          <body className={inter.className}>{children}</body>
+          <body className={cn(inter.className, spaceGrotesk.className)}>
+            {children}
+          </body>
         </ReactQueryProvider>
       </NextIntlClientProvider>
     </html>
